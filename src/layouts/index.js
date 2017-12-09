31014670a719c025644c460217735daf7ad558cb
David <dgfreecodecamper@gmail.com>
2017-12-09 23:18:08 +0000

    revert to css and add grid-css for responsive layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,15 +4,16 @@ import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
 // import './index.css'
-// import './index.scss'
+import './index.css'
 
 const SideMenu = () => {
   const sideMenuStyle = {
     backgroundColor: 'green'
   };
   return(
-    <div>
+    <div style={sideMenuStyle} className="menu">
       <ul>
+        <li>the menu</li>
         <li>chapter 1</li>
         <li>chapter 2</li>
         <li>chapter 3</li>
@@ -32,8 +33,9 @@ const Header = () =>  {
     backgroundColor: 'red'
   };
   return (
-    <div style={headerStyle}>
-      <h1>hel44pt</h1>
+    <div style={headerStyle} className="header">
+      <h1>Hello this is the header</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste aliquid explicabo possimus maiores fugiat officiis magnam veritatis laborum expedita at. Quidem debitis hic nam illo eius explicabo. Laboriosam impedit amet ducimus temporibus dolorum, corrupti corporis veritatis possimus aliquid, minus reprehenderit debitis enim atque sit magni similique obcaecati unde! Officiis, rem.</p>
     </div>
   )
 }
@@ -45,23 +47,24 @@ const Footer = () => {
     backgroundColor: 'blue'
   }
   return (
-    <div style={footerStyle}>
-      <h1>Footer</h1>
+    <div style={footerStyle} className="footer">
+      <h1>Hello for the footer</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum saepe maxime libero tempore temporibus. Molestias harum placeat, ex est amet dolor cupiditate quidem quos dolores natus similique illum architecto non totam voluptate necessitatibus illo eligendi quis corporis aliquid iusto praesentium suscipit odio fugit. Nesciunt voluptates officiis minus odio praesentium vel, laudantium dolore, tempora voluptatem consequatur illo, nostrum velit porro eum!</p>
     </div>
   )
 }
 
 
-const Mainsect = () => {
-  const mainsectStyle = {
-    backgroundColor: 'pink'
-  }
-  return (
-    <div>
-
-    </div>
-  )
-}
+// const Mainsect = ({ children }) => {
+//   const mainsectStyle = {
+//     backgroundColor: 'pink'
+//   }
+//   return (
+//     <div>
+//       {children()}
+//     </div>
+//   )
+// }
 
 
 
@@ -77,16 +80,23 @@ const TemplateWrapper = ({ children }) => (
       ]}
     >
     </Helmet>
-
-    <div className="container">
-      <Header />
-      <div>
-        {children()}
+    <div className="pagewrapper">
+      <div className="container">
+        <Header />
+        {/* <Mainsect /> */}
+        <div className="content" style={{ backgroundColor: 'pink' }}>
+          {children()}
+        </div>
+        <SideMenu />
+        <Footer />
       </div>
-      <Footer/>
     </div>
   </div>
 )
+
+// Mainsect.propTypes = {
+//   children: PropTypes.func,
+// }
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
